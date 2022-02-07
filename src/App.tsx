cde392378beb9dcb9152c130c46af37980625620
ega4432 <yoshimitsu.egashira@ibm.com>
2022-02-07 14:25:16 +0900

    add: device orientation event

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [isOk, setIsOk] = useState<boolean>(false);
+  const [alpha, setAlpha] = useState<number | null>(null);
+  const [beta, setBeta] = useState<number | null>(null);
+  const [gamma, setGamma] = useState<number | null>(null);
+
+  const requestPermission = () => {
+    if (window.DeviceOrientationEvent) {
+      console.log(DeviceOrientationEvent.prototype);
+      setAlpha(DeviceOrientationEvent.prototype.alpha);
+      setBeta(DeviceOrientationEvent.prototype.beta);
+      setGamma(DeviceOrientationEvent.prototype.gamma);
+      setIsOk(true);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          温度を測定します。
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={requestPermission}
         >
-          Learn React
-        </a>
+          許可する
+        </button>
       </header>
+      {isOk && (
+        <div>
+          <ul>
+            <li>alpha: {alpha}</li>
+            <li>beta: {beta}</li>
+            <li>gamman: {gamma}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
